6cd92e6743a7421551cf16488adb21c7f5ca5094
chocolaggibbiddori <115144342+chocolaggibbiddori@users.noreply.github.com>
2024-03-21 23:19:31 +0900

    Feat: 설정 파일을 읽어 Setting 객체 셋업하는 로직 추가

--- src/main/java/com/dongs/Main.java
+++ src/main/java/com/dongs/Main.java
@@ -3,6 +3,8 @@ package com.dongs;
 import com.dongs.scheduler.CSVReader;
 import com.dongs.scheduler.Schedule;
 import com.dongs.scheduler.Scheduler;
+import com.dongs.settings.InvalidExtensionException;
+import com.dongs.settings.Yaml;
 import lombok.extern.java.Log;
 
 import java.io.FileNotFoundException;
@@ -12,14 +14,18 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        String filePath = "src/main/resources/data.csv";
+        String configPath = "src/main/resources/config.yml";
+        String dataPath = "src/main/resources/data.csv";
 
         try {
-            List<Schedule> scheduleList = CSVReader.readSchedulesFromCSV(filePath);
+            Yaml.readSettings(configPath);
+            List<Schedule> scheduleList = CSVReader.readSchedulesFromCSV(dataPath);
             Scheduler scheduler = Scheduler.getInstance();
             scheduler.add(scheduleList);
         } catch (FileNotFoundException e) {
             log.info("There is no file");
+        } catch (InvalidExtensionException e) {
+            log.info(e.getMessage());
         }
 
         System.out.println("Finish!");
