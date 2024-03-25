8c4f809380224a38d780f06c5d0969558d868b9c
chocolaggibbiddori <115144342+chocolaggibbiddori@users.noreply.github.com>
2024-03-25 23:57:44 +0900

    Test: [DefaultScheduler] readAndInspect 메서드 테스트 작성
    리팩토링 등 수정이 빈번하므로 테스트 케이스를 만듦
    1. 매개변수가 null 값인 경우
    2. 데이터 파일이 없는 경우
    3. 데이터 파일이 csv 파일이 아닌 경우
    4. 일정이 충돌나는 경우

---
+++ src/test/java/com/dongs/scheduler/DefaultSchedulerTest.java
@@ -0,0 +1,71 @@
+package com.dongs.scheduler;
+
+import com.dongs.common.exception.InvalidExtensionException;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.io.FileNotFoundException;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class DefaultSchedulerTest {
+
+    private final DefaultScheduler scheduler = DefaultScheduler.getInstance();
+
+    @AfterEach
+    void tearDown() {
+        scheduler.values().forEach(Set::clear);
+    }
+
+    @Test
+    @DisplayName("매개변수가 null")
+    void readAndInspect_throw_argIsNull() {
+        assertThatThrownBy(() -> scheduler.readAndInspect(null))
+                .hasMessage("csvPath is null")
+                .isInstanceOf(NullPointerException.class);
+    }
+
+    @Test
+    @DisplayName("데이터 파일이 없음")
+    void readAndInspect_throw_noFile() {
+        String csvPath = getPath("no-file.csv");
+        assertThatThrownBy(() -> scheduler.readAndInspect(csvPath))
+                .isInstanceOf(FileNotFoundException.class);
+    }
+
+    @Test
+    @DisplayName("csv 파일이 아님")
+    void readAndInspect_throw_invalidExtension() {
+        String csvPath = getPath("file.yml");
+        assertThatThrownBy(() -> scheduler.readAndInspect(csvPath))
+                .isInstanceOf(InvalidExtensionException.class);
+    }
+
+    @ParameterizedTest(name = "filename={0}, answer={1}")
+    @CsvSource({"data1.csv, 0", "data2.csv, 1", "data3.csv, 4"})
+    @DisplayName("일정 충돌")
+    void readAndInspect_clash(String filename, int answer) throws FileNotFoundException, InvalidExtensionException {
+        //g
+        String csvPath = getPath(filename);
+
+        //w
+        scheduler.readAndInspect(csvPath);
+
+        //t
+        int scheduleNum = scheduler.values().stream()
+                .mapToInt(Set::size)
+                .sum();
+
+        assertThat(scheduleNum).isEqualTo(answer);
+    }
+
+    private String getPath(String filename) {
+        String prefix = "src/test/resources/scheduler/default-scheduler/";
+        return prefix + filename;
+    }
+}
