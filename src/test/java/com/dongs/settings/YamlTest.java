b21666e128333262b2bfcb6517880b3d04f41ec3
chocolaggibbiddori <115144342+chocolaggibbiddori@users.noreply.github.com>
2024-03-26 21:20:01 +0900

    Test: 추가된 설정 객체 테스트

--- src/test/java/com/dongs/settings/YamlTest.java
+++ src/test/java/com/dongs/settings/YamlTest.java
@@ -1,17 +1,25 @@
 package com.dongs.settings;
 
 import com.dongs.common.exception.InvalidExtensionException;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.FileNotFoundException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class YamlTest {
 
     @Test
     void readSettings() throws InvalidExtensionException, FileNotFoundException {
+        //g
         String configPath = "src/test/resources/config.yml";
+
+        //w
         Setting setting = Yaml.readSettings(configPath);
-        Assertions.assertTrue(setting.schedule().autoRemove().value());
+
+        //t
+        assertTrue(setting.schedule().autoRemove().value());
+        assertThat(setting.data().filename().value()).isEqualTo("config");
     }
 }
